dab7a19d285d3018a1d5e17ecfc604bb7be2417b
Thomas <tehrhard@uni-mainz.de>
2015-06-05 18:39:02 +0200

    Allow for 2d grid scan, using the hybrid Fisher / pull method for the remaining parameters.

--- pisa/analysis/scan/RunPullMethod.py
+++ pisa/analysis/scan/RunPullMethod.py
@@ -2,9 +2,12 @@
 #
 # RunPullMethod.py
 #
-# Scans over the space of atmospheric (theta23) oscillation parameters,
+# Scans a 1- or 2-d parameter grid (with theta23 being one of them),
 # analytically minimising the chisquare over the other - linearised - parameters
-# by calculating the Fisher matrix at each point on the grid.
+# by calculating the Fisher matrix at each point and determining the pulls.
+# Alternatively, can also make use of numerical minimisation at each point, which is
+# slower by a factor of on the order of 10. In addition to the chisquare, also returns
+# the best fits of all parameters which are optimised.
 #
 # author: Thomas Ehrhardt - tehrhard@uni-mainz.de
 #
@@ -25,7 +28,7 @@ from pisa.analysis.fisher.FisherAnalysis import get_fisher_matrices
 
 from pisa.utils.log import logging, profile, physics, set_verbosity
 from pisa.utils.jsons import from_json,to_json
-from pisa.utils.params import get_values, select_hierarchy, fix_atm_params, get_atm_params
+from pisa.utils.params import get_values, select_hierarchy, fix_atm_params, get_atm_params, get_free_params, get_param_priors, get_prior_chisquare
 from pisa.utils.utils import Timer
 
 parser = ArgumentParser(
@@ -34,11 +37,13 @@ parser = ArgumentParser(
 parser.add_argument('-t','--template_settings',type=str,
                     metavar='JSONFILE', required = True,
                     help='''Settings related to the template generation and systematics.''')
+parser.add_argument('-p2','--2nd-parameter', type=str, required=False, dest='scan_par2',
+		    help='''Scan a 2-d grid, with theta23 and this parameter.''')
 parser.add_argument('-m','--minimizer_settings',type=str,
                     metavar='JSONFILE', required = False,
                     help='''Settings related to the optimizer used in the LLR analysis.''')
 parser.add_argument('-g','--grid_settings',type=str,metavar='JSONFILE', required = True,
-                    help='''Get chisquare at defined oscillation parameter grid values,
+                    help='''Get chisquare at defined grid values,
                     according to these input settings.''')
 parser.add_argument('-s','--save-steps',action='store_true',default=False,
                     dest='save_steps',
@@ -64,28 +69,36 @@ grid_settings = from_json(args.grid_settings)
 
 channel = template_settings['params']['channel']['value']
 #Workaround for old scipy versions
-import scipy
-if scipy.__version__ < '0.12.0':
+if minimizer_settings is not None:
+  import scipy
+  if scipy.__version__ < '0.12.0':
     logging.warn('Detected scipy version %s < 0.12.0'%scipy.__version__)
     if 'maxiter' in minimizer_settings:
-        logging.warn('Optimizer settings for \"maxiter\" will be ignored')
-        minimizer_settings.pop('maxiter')
+      logging.warn('Optimizer settings for \"maxiter\" will be ignored')
+      minimizer_settings.pop('maxiter')
 
 if args.gpu_id is not None:
-    template_settings['params']['gpu_id'] = {}
-    template_settings['params']['gpu_id']['value'] = args.gpu_id
-    template_settings['params']['gpu_id']['fixed'] = True
+  template_settings['params']['gpu_id'] = {}
+  template_settings['params']['gpu_id']['value'] = args.gpu_id
+  template_settings['params']['gpu_id']['fixed'] = True
 
 # Get the parameters
 orig_params = template_settings['params'].copy()
-hypo_params = hypo_settings['params'].copy()
+orig_hypo_params = hypo_settings['params'].copy()
 
-# Make sure that theta23 is fixed:
+# Make sure that theta23 is fixed (+ possibly 2nd parameter):
 logging.warn("Ensuring that theta23 is fixed for this analysis")
-for key, value in orig_params.items():
-  if 'theta23' in key:
-    orig_params[key]['fixed'] = True
-#print "params: ",params.items()
+
+if args.scan_par2 is not None:
+  logging.warn("Ensuring that %s is fixed for this analysis"%args.scan_par2)
+  for key, value in orig_params.items():
+    if 'theta23' in key or args.scan_par2 in key:
+      orig_params[key]['fixed'] = True
+else:
+  for key, value in orig_params.items():
+    if 'theta23' in key:
+      orig_params[key]['fixed'] = True
+
 
 with Timer() as t:
     template_maker = TemplateMaker(get_values(orig_params),**template_settings['binning'])
@@ -104,35 +117,42 @@ for data_tag, data_normal in data_types:
   data_params = select_hierarchy(orig_params, normal_hierarchy=data_normal)
   asimov_data_set = get_asimov_fmap(template_maker, get_values(data_params),
                                     chan=channel)
+
   results[data_tag]['asimov_data'] = asimov_data_set
+
   hypo_types = [('hypo_NMH',True)]#,('hypo_IMH',False)]
   for hypo_tag, hypo_normal in hypo_types:
 
     hypo_params = select_hierarchy(orig_params, normal_hierarchy=hypo_normal)
-    # Calculate steps for theta23
-    t23_params = { 'theta23': hypo_params['theta23'] }
-    calc_steps( t23_params, grid_settings['steps'] )
+    # Calculate steps for theta23 (+ possibly additional parameter)
+    if args.scan_par2 is not None:
+      free_params = { 'theta23': hypo_params['theta23'], args.scan_par2: hypo_params[args.scan_par2] }
+    else:
+      free_params = { 'theta23': hypo_params['theta23'] }
+
+    calc_steps( free_params, grid_settings['steps'] )
 
     # Build a list from all parameters that holds a list of (name, step) tuples
-    steplist = [ step for step in t23_params['theta23']['steps'] ]
+    steplist = [ [(name, step) for step in free_params[name]['steps']] for name in sorted(free_params.keys()) ]
 
-    print "steplist: ",steplist
-    # Prepare to store all the steps
-    steps = { key:[] for key in t23_params.keys() }
+    # Prepare to store all the steps, chisquare values and best fits
+    steps = { key:[] for key in free_params.keys() }
     steps['chisquare'] = []
+    best_fits = { key:[] for key in get_free_params((data_params))  }
 
     # Iterate over the cartesian product, and set fixed parameter to value
-    for step in steplist:
-      print "running at %.2f"%step
+    for pos in product(*steplist):
+    #for step in steplist:
+      print "running at ",pos
       # be cautious...
       hypo_params_new = {}
       for (k, v) in hypo_params.items():
 	hypo_params_new[k]=v.copy()
-        if k=='theta23':
-	  hypo_params_new[k]['value'] = step
+	if k in free_params.keys():
 	  hypo_params_new[k]['fixed'] = True
-
-      steps['theta23'].append(step)
+	  param_val = pos[sorted(free_params.keys()).index(k)][1]
+	  hypo_params_new[k]['value'] = param_val
+	  steps[k].append(param_val)
 
       if minimizer_settings is not None:
         with Timer() as t:
@@ -140,6 +160,12 @@ for data_tag, data_normal in data_types:
                                               minimizer_settings, args.save_steps,
                                               normal_hierarchy=hypo_normal)
         profile.info("==> elapsed time for optimizer: %s sec"%t.secs)
+
+	# store the best fit values
+	for p in chi2_data:
+	  if p!='chisquare':
+	    best = chi2_data[p][0]
+	    best_fits[p].append(best)
 
       else: # Make use of the pull method
 	# First, we need the template settings for truth and hypo.
@@ -151,7 +177,16 @@ for data_tag, data_normal in data_types:
 
 	min_chi2_vals = { }
 	for p in pulls[data_tag][hypo_tag]:
-	  min_chi2_vals[p[0]] = hypo_params_new[p[0]]['value'] + p[1]
+	  best = hypo_params_new[p[0]]['value'] + p[1]
+	  min_chi2_vals[p[0]] = best
+	  #print "%s best fit: %s"%(p[0], best)
+          best_fits[p[0]].append(best)
+
+	# artificially add the parameter(s) scanned over to this dictionary, so
+	# we can add the prior penalty terms for these
+
+	for scanned_par in free_params:
+	  min_chi2_vals[scanned_par] = hypo_params_new[scanned_par]['value']
 
 	min_chi2_params_new = { }
 	for (k, v) in hypo_params_new.items():
@@ -162,13 +197,16 @@ for data_tag, data_normal in data_types:
 	min_chi2_hypo_data_set = get_asimov_fmap(template_maker, get_values(min_chi2_params_new),
                                     chan=channel)
 	chi2_data = {}
-	chi2_data['chisquare'] = [get_binwise_chisquare(asimov_data_set, min_chi2_hypo_data_set)]
-		
+	chi2 = get_binwise_chisquare(asimov_data_set, min_chi2_hypo_data_set)
+	priors = get_param_priors(get_free_params(select_hierarchy(orig_hypo_params,hypo_normal)))
+	#print sorted(min_chi2_vals.items()), priors
+	chi2 += sum([ get_prior_chisquare(opt_val, sigma, value)
+		      for (opt_val, (sigma, value)) in zip([ opt for (p, opt) in sorted(min_chi2_vals.items())], priors) ])
+	chi2_data['chisquare'] = [chi2]
 	
       steps['chisquare'].append(chi2_data['chisquare'][-1])
+      steps['best_fits'] = best_fits
 
-      # Then save the minimized free params later??
-      #print "\n\nsteps: ",steps
       results[data_tag][hypo_tag] = steps
 
 logging.warn("FINISHED. Saving to file: %s"%args.outfile)
